acffc59b9039a528a36c5b2ec8fcfa034aea86eb
Luis-Miguelj <luismiguel.dev01@gmail.com>
2025-01-08 15:07:27 -0300

    Modificações no banco

--- src/types/env.ts
+++ src/types/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   JWT_SECRET: z.string(),
-  DIRECT_URL: z.string(),
+  // DIRECT_URL: z.string(),
   DATABASE_URL: z.string(),
   COOKIE_SECRET: z.string(),
   // PORT: z.number().optional(),
